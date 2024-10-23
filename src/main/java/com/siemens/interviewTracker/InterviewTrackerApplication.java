db57b426b5d0501d97daa89594a86029e9b4e9b6
Zaituny <mohamedinv.580@gmail.com>
2024-10-23 23:35:02 +0300

    added a dummy end point for testing

--- src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
+++ src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
@@ -2,10 +2,17 @@ package com.siemens.interviewTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class InterviewTrackerApplication {
-
+	@GetMapping("/dummyEndPoint")
+	public String dummyEndPoint() {
+		return "Hello from dummyEndPoint";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewTrackerApplication.class, args);
 	}
