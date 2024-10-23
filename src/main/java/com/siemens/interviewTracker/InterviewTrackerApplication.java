08bedac1a1ee2ff6774874880c609b0fafecb342
habibaahmed11 <habibaahmed3372@gmail.com>
2024-10-23 19:58:25 +0300

    Implement database creation and connection

--- src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
+++ src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InterviewTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
+        System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
+        System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
+        SpringApplication.run(InterviewTrackerApplication.class, args);
+    }
 
 }
