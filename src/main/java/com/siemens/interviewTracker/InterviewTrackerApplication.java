cd07afef0f305cf2f6c80afc8bf36c44feb39074
habibaahmed11 <habibaahmed3372@gmail.com>
2024-10-27 17:29:21 +0300

    swagger doc

--- src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
+++ src/main/java/com/siemens/interviewTracker/InterviewTrackerApplication.java
@@ -3,14 +3,11 @@ package com.siemens.interviewTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InterviewTrackerApplication {
 
     public static void main(String[] args) {
-        System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
-        System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
-        System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
         SpringApplication.run(InterviewTrackerApplication.class, args);
     }
-
 }
