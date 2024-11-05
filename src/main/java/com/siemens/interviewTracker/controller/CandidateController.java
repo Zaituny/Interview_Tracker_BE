63cf4dd0b781ef819860f2f8d8af00a8d51ebd52
Zaituny <mohamedinv.580@gmail.com>
2024-11-05 17:48:30 +0200

    created controller and service class for candidate

---
+++ src/main/java/com/siemens/interviewTracker/controller/CandidateController.java
@@ -0,0 +1,12 @@
+package com.siemens.interviewTracker.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/api/v0/candidates")
+public class CandidateController {
+
+}
---
+++ src/main/java/com/siemens/interviewTracker/service/CandidateService.java
@@ -0,0 +1,9 @@
+package com.siemens.interviewTracker.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CandidateService {
+}
